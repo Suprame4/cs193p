66deb5a0ca5e5ee473cf15c1c5b890cecc028b93
Adrian G <adriancortesgonz@gmail.com>
2025-05-11 18:34:34 -0700

    Complete card game and lecture 2

--- lecture1/Memorize/Memorize/ContentView.swift
+++ lecture1/Memorize/Memorize/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 // ContentView conforms to the View protocol
 struct ContentView: View {
     
+    let emojis: [String] = ["👻","🕷️","🎃","😈","👻","🕷️","🎃","😈"]
+    @State var cardCount = 4
     /*
      the value of the body variable is not
      stored anywhere, it's computed,
@@ -20,32 +22,75 @@ struct ContentView: View {
      */
     var body: some View {
         // VStack(content: { ... })
-        HStack {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView(isFaceUp: true)
-            CardView()
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardAdjuster
         }
-        .foregroundColor(.orange)
         .padding()
     }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self){ index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .foregroundColor(.orange)
+    }
+    
+    var cardAdjuster: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+    }
+    // you must use some View for a computed property like cardRemover if you want the return type to be treated as a View
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
 }
-//comment
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    let content: String
+// a state variable allows the view to be recreated dynamically at runtime, without needing to recompile the code
+    @State var isFaceUp: Bool = false
     
     var body: some View {
-        if isFaceUp {
-            ZStack(content: {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
-                Text("👻").font(.largeTitle)
-            })
-        } else {
-            RoundedRectangle(cornerRadius: 12)
+// inside the view, you can only do if, lists, and variable assignments
+        let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+        ZStack {
+            Group {
+                // trailing closure syntax is applied to ZStack
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+//booleans are struct, meaning they have methods associated - isFaceUp.toggle() would also work
         }
     }
 }
@@ -56,3 +101,4 @@ struct CardView: View {
 /*
  Every UI element in SwiftUI is a struct that conforms to the View protocol
  */
+
